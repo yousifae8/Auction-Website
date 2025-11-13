f099c044944e5d3789a8cd0f5054f6a769a57ec2
yousifae8 <yousifae8@gmail.com>
2025-11-13 10:13:16 +0200

    cleaned the code from comments, and added add product form page that can add data to the json file data, made and increasing id number class

--- app.py
+++ app.py
@@ -3,30 +3,46 @@ import json
 
 app = Flask(__name__)
 
-#my default products path
+
 data = "Products/products.json"
 
-#a product route with bid bar
+class idCounter():
+    def __init__(self, id=0):
+        self.id = id
+    def id_current_num(self):
+        return self.id
+
+        pass
+    def id_increase(self):
+         self.id += 1
+         return self.id
+
+id_num = idCounter(0)
+
+
+
+
+
 @app.route("/products/<int:product_id>", methods=["GET", "POST"])
 def details(product_id):
     det = products()
     product = None
 
-    # a loop that checks for the id of the choosen product
+   
     for i in det:
         if i["id"] == product_id:
             product = i
             break
-    #if not, it will display not found
+   
     if not product:
         return "Product not found", 404
 
-    #receiving bid offers fron the user
+
     if request.method == "POST":
         bid = request.form.get("bid")
         try:
             bid = float(bid)
-            #checks if the bid is higher or not
+            
             if bid > product["high"]:
                 product["high"] = bid
                 with open(data, "w") as file:
@@ -43,10 +59,47 @@ def details(product_id):
     return render_template("products.html", product=product)
 
 
-#this function loads the product data
 def products():
     with open(data, "r") as products:
         return json.load(products)
+
+def json_data(id,name,description,price,link):
+    new_product = {
+        "id": id_num.id_increase(),
+        "name": name,
+        "description": description,
+        "price": float(price),
+        "high": float(price),
+        "link": link
+    }
+    return new_product
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        name = request.form.get("name")
+        description = request.form.get("description")
+        price = request.form.get("price")
+        link = request.form.get("link")
+
+        user_data = json_data(id_num,name,description,price,link)
+        with open(data) as products:
+            content = json.load(products)
+
+        content.append(user_data)
+        with open(data, "w") as file:
+            json.dump(content, file, indent=4)
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
+
+
+
+
+
+
+
 
 
 @app.route("/")
